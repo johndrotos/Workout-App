b14ba9ee5a135e76e7dcf28ef7eb5954248bab1d
John Drotos <johndrotos3@gmail.com>
2024-05-08 02:44:59 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://johndrotos3:Menchies.com3@cluster0.1r7st2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 app.listen(3001, () => {
     console.log("SERVER IS RUNNING");
 });
+
